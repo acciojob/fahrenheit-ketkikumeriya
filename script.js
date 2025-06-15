25fbb43b08fdfd368922c4203347dd75d44ccf23
ketki kumeriya <111123802+ketkikumeriya@users.noreply.github.com>
2025-06-15 19:59:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function toFahrenheit(celsius) {
  // Write your code here
-	    let fahrenheit = (celsius * 9/5) + 32;
+	let fahrenheit = (celsius * 9/5) + 32;
+	console.log(`Celsius: ${celsius}, Fahrenheit: ${fahrenheit}`);
     return parseFloat(fahrenheit.toFixed(2));
 
 }
 
 // Do not change the code below
 const celsius = prompt("Enter Celsius:");
+console.log(`Input Celsius: ${celsius}`);
 alert(toFahrenheit(Number(celsius)));
