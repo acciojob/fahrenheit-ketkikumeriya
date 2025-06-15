5f5452c50a673972dcd4ab477ead28307025b3e3
ketki kumeriya <111123802+ketkikumeriya@users.noreply.github.com>
2025-06-15 19:50:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function toFahrenheit(celsius) {
  // Write your code here
+	    let fahrenheit = (celsius * 9/5) + 32;
+    return parseFloat(fahrenheit.toFixed(2));
+
 }
 
 // Do not change the code below
